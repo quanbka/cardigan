331ce78e08705218fcf4771b5d7c567ed82f4f2a
Le Van Quan <quanbka.cntt@gmail.com>
2021-05-25 14:25:16 +0700

    trang chu

--- wp-content/themes/lep/footer.php
+++ wp-content/themes/lep/footer.php
@@ -1,34 +1,247 @@
-<?php
-/**
- * The template for displaying the footer
- *
- * Contains the closing of the #content div and all content after.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package lep
- */
+<footer class="footer   ">
 
-?>
+	<div class="top-footer">
+		<div class="container-fluid">
+			<div class="row">
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'lep' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'lep' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'lep' ), 'lep', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
 
-<?php wp_footer(); ?>
+
+				<div class="col-xs-12 col-sm-12 col-md-4">
+					<div class="area_phone_contact">
+						<p class="number_phone">
+							<i class="fa fa-phone "></i>
+							<span>Bất cứ khi nào nàng cần, đã có Lep' hỗ trợ nàng 24/7!</span>
+							<a href="tel:0393644116">
+								0393644116
+							</a>
+						</p>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+	<div class="title-expand visible-xs text-center">
+		<a class="btn-fter" data-toggle="collapse" href="#fter-content" role="button" aria-expanded="true" aria-controls="fter-content">
+			Thông tin khác<span class="fa fa-angle-down"> </span>
+		</a>
+	</div>
+	<div class="main-footer collapse" id="fter-content">
+		<div class="container-fluid">
+			<div class="row">
+
+				<div class="col-xs-12 col-sm-6 col-md-4 col-lg">
+					<div class="footer-col footer-content1">
+						<h4 class="footer-title">
+							Giới thiệu
+						</h4>
+						<div class="footer-content">
+
+							<p>Váy của Lep’ không dành cho các cô gái “ngoan” luôn e dè sợ sệt. Váy của Lep’ là những em váy hoa nhí nhìn là đã thấy rực rỡ yêu đời. </p>
+
+
+							<div class="logo-footer">
+								<a href="http://online.gov.vn/Home/WebDetails/75961" target="_blank" rel="nofollow noreferrer">
+									<img src="https://file.hstatic.net/200000258383/file/logosalenoti_e846b47ea6254c96a889125e1c53cf5c.png" alt="Bộ Công Thương">
+								</a>
+							</div>
+
+						</div>
+					</div>
+				</div>
+
+
+				<div class="col-xs-12 col-sm-6 col-md-2 col-lg">
+					<div class="footer-col footer-block">
+						<h4 class="footer-title">
+							Liên kết
+						</h4>
+						<div class="footer-content toggle-footer">
+							<ul>
+
+								<li class="item">
+									<a href="/search" title="Tìm kiếm">Tìm kiếm</a>
+								</li>
+
+								<li class="item">
+									<a href="/pages/about-us" title="Giới thiệu">Giới thiệu</a>
+								</li>
+
+								<li class="item">
+									<a href="/pages/chinh-sach-doi-tra" title="Chính sách đổi trả">Chính sách đổi trả</a>
+								</li>
+
+								<li class="item">
+									<a href="/pages/chinh-sach-bao-mat-1" title="Chính sách bảo mật">Chính sách bảo mật</a>
+								</li>
+
+								<li class="item">
+									<a href="/pages/dieu-khoan-dich-vu" title="Điều khoản dịch vụ">Điều khoản dịch vụ</a>
+								</li>
+
+								<li class="item">
+									<a href="/pages/chinh-sach-mua-hang" title="Chính sách mua hàng">Chính sách mua hàng</a>
+								</li>
+
+								<li class="item">
+									<a href="/pages/chinh-sach-van-chuyen" title="Chính sách vận chuyển">Chính sách vận chuyển</a>
+								</li>
+
+								<li class="item">
+									<a href="/pages/chinh-sach-thanh-toan" title="Chính sách thanh toán">Chính sách thanh toán</a>
+								</li>
+
+							</ul>
+						</div>
+					</div>
+				</div>
+
+
+				<div class="col-xs-12 col-sm-6 col-md-3 col-lg">
+					<div class="footer-col">
+						<h4 class="footer-title">
+							CÔNG TY TNHH THỜI TRANG SONG BÌNH
+						</h4>
+						<div class="footer-content footer-contact">
+							<ul>
+								<li class="contact-1">Số 61, ngách 41, ngõ 164 Vương Thừa Vũ, phường Khương Trung, quận Thanh Xuân, thành phố Hà Nội, Việt Nam</li>
+								<li class="contact-2">0393644116</li>
+								<li class="contact-3">Số giấy ĐKKD 0108175559, do Sở Kế hoạch và Đầu tư thành phố Hà Nội cấp ngày 08/03/2018</li>
+								<li class="contact-4">info@lep.vn</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+
+
+				<div class="col-xs-12 col-sm-6 col-md-3 col-lg">
+					<div class="footer-col">
+						<h4 class="footer-title">
+							Fanpage
+						</h4>
+						<div class="footer-content footer-contact">
+							<!-- Facebook widget -->
+							<div class="footer-static-content">
+								<div class="fb-page" data-href="https://www.facebook.com/Lep.by.Tram"  data-height="300" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" data-show-posts="false">	</div>
+							</div>
+							<div style="clear:both;" ></div>
+
+							<!-- #Facebook widget -->
+							<script>
+								setTimeout(function(){
+									(function(d, s, id) {
+										var js, fjs = d.getElementsByTagName(s)[0];
+										if (d.getElementById(id)) return;
+										js = d.createElement(s); js.id = id;
+										js.src = "//connect.facebook.net/en_US/sdk/xfbml.customerchat.js#xfbml=1&version=v2.0";
+										fjs.parentNode.insertBefore(js, fjs);
+									}(document, 'script', 'facebook-jssdk'));
+								},5000);
+							</script>
+
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+	<div class="bottom-footer text-center">
+		<div class="container-fluid">
+			<p>Copyright © 2021 <a href="https://lep.vn"> Lep' Dress</a>.</p>
+		</div>
+	</div>
+</footer>
+
+			<div class="back-to-top hidden">
+				<a href="javascript:void(0);">
+					<div class="btt-back">
+						<span class="btt-label-back">back to top</span>
+						<span class="btt-icon-back"><i class="fa fa-long-arrow-up"></i></span>
+					</div>
+				</a>
+			</div>
+			<div id="site-overlay" class="site-overlay"></div>
+		</div>
+		<script src='//hstatic.net/0/0/global/option_selection.js' type='text/javascript'></script>
+<script src='//hstatic.net/0/0/global/api.jquery.js' type='text/javascript'></script>
+<script src='//theme.hstatic.net/200000258383/1000664579/14/plugins.js?v=728' type='text/javascript'></script>
+
+<!-- haravan app -->
+<!-- END - haravan app -->
+<script src='//theme.hstatic.net/200000258383/1000664579/14/scripts.js?v=728' type='text/javascript'></script>
+<!-- POPUP LOAD -->
+<!--END *** POPUP LOAD =====================-->
+
+
+
+<script>
+	setTimeout(function(){
+		animation_check();
+	}, 100);
+	function animation_check(){
+		var scrollTop = $(window).scrollTop() - 300;
+		$('.animation-tran').each(function(){
+			if($(this).offset().top < scrollTop + $(window).height()){
+				$(this).addClass('active');
+			}
+		})
+	}
+	$(window).scroll(function(){
+		//setTimeout(function(){
+		animation_check();
+		//}, 500);
+	})
+</script>
+		<div class="flexsocial">
+
+
+
+
+
+	<div class="item">
+		<a href="https://www.m.me/Lep.by.Tram/"><img src="//theme.hstatic.net/200000258383/1000664579/14/anhicon1.png?v=728" alt="social" /></a>
+	</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+<style>
+	.flexsocial{
+	display: -webkit-box;      /* OLD - iOS 6-, Safari 3.1-6 */
+	display: -moz-box;         /* OLD - Firefox 19- (buggy but mostly works) */
+	display: -ms-flexbox;      /* TWEENER - IE 10 */
+	display: -webkit-flex;     /* NEW - Chrome */
+	display: flex;
+	flex-wrap:wrap;
+	bottom: 40px;
+	z-index: 999;
+	width: 59px;
+  position: fixed;
+  flex-direction: column-reverse;
+	right: 10px;
+	}
+	.flexsocial > div{
+	width:60px;
+	text-align:center;
+margin-top:25px;
+	}
+	.flexsocial img{
+	width:50px;
+	height:50px;
+	}
+</style>
 
 </body>
 </html>
